46c805375d44c3de1ba2754ff18677e0fe5467e0
Fuechschen <me@fuechschen.org>
2016-03-15 20:02:12 +0100

    ups...

--- models/CustomCommand.js
+++ models/CustomCommand.js
@@ -1,6 +1,6 @@
 module.exports = function(sequelize, Sequelize) {
     return sequelize.define("CustomCommand", {
-        id: {type: Sequelize.INTEGER.UNSIGNED, primaryKey: true, unique: true},
+        id: {type: Sequelize.INTEGER.UNSIGNED, primaryKey: true, unique: true, autoIncrement: true},
         trigger: {type: Sequelize.STRING, allowNull: false},
         message: {type: Sequelize.STRING, allowNull: false},
         status: {type: Sequelize.BOOLEAN, defaultValue: true}
