2bdfef5992b391604586e39b4d8b99bed32c602d
Fuechschen <me@fuechschen.org>
2016-06-30 03:23:05 +0200

    refractoring event system to be more extensible

--- bot.js
+++ bot.js
@@ -1,7 +1,10 @@
-var config = require('./lib/load_config.js');
-var storyboard = require('storyboard');
-var langfile = require('./langfile.js');
 var moment = require('moment');
+var storyboard = require('storyboard');
+var fs = require('fs');
+
+var config = require('./lib/load_config.js');
+var langfile = require('./langfile.js');
+var story = storyboard.mainStory;
 
 storyboard.config({filter: '*:' + config.options.loglevel});
 storyboard.mainStory.info('Starting plugbot version ' + require('./package.json').version);
@@ -18,42 +21,26 @@ redis.exists('meta:data:staff:active').then(function (ex) {
     if (ex === 0) redis.set('meta:data:staff:active', 1);
 });
 
-plugged.on(plugged.SOCK_CLOSED, require('./lib/eventhandlers/sock_closed'));
-plugged.on(plugged.SOCK_ERROR, require('./lib/eventhandlers/sock_error'));
-
-plugged.on(plugged.LOGIN_SUCCESS, require('./lib/eventhandlers/login_success'));
-plugged.on(plugged.LOGIN_ERROR, require('./lib/eventhandlers/login_error'));
-
-plugged.on(plugged.CONN_SUCCESS, require('./lib/eventhandlers/conn_success'));
-plugged.on(plugged.CONN_PART, require('./lib/eventhandlers/conn_part'));
-plugged.on(plugged.CONN_ERROR, require('./lib/eventhandlers/conn_error'));
-plugged.on(plugged.CONN_WARNING, require('./lib/eventhandlers/conn_warning'));
-
-plugged.on(plugged.PLUG_MESSAGE, require('./lib/eventhandlers/plug_message'));
-plugged.on(plugged.PLUG_ERROR, require('./lib/eventhandlers/plug_error'));
-plugged.on(plugged.KILL_SESSION, require('./lib/eventhandlers/kill_session'));
-plugged.on(plugged.PLUG_UPDATE, require('./lib/eventhandlers/plug_update'));
-plugged.on(plugged.MAINTENANCE_MODE, require('./lib/eventhandlers/maintenance_mode'));
-
-plugged.on(plugged.JOINED_ROOM, require('./lib/eventhandlers/joined_room'));
-
-plugged.on(plugged.ADVANCE, require('./lib/eventhandlers/advance'));
-plugged.on(plugged.WAITLIST_UPDATE, require('./lib/eventhandlers/waitlist_update'));
-
-plugged.on(plugged.FRIEND_JOIN, require('./lib/eventhandlers/user_join'));
-plugged.on(plugged.USER_JOIN, require('./lib/eventhandlers/user_join'));
-
-plugged.on(plugged.USER_LEAVE, require('./lib/eventhandlers/user_leave'));
-
-plugged.on(plugged.CHAT, require('./lib/eventhandlers/chat'));
-plugged.on(plugged.CHAT_MENTION, require('./lib/eventhandlers/chat'));
-plugged.on(plugged.CHAT_COMMAND, require('./lib/eventhandlers/chat_command'));
-
-plugged.on(plugged.VOTE, require('./lib/eventhandlers/vote'));
-
-plugged.on(plugged.MOD_STAFF, require('./lib/eventhandlers/mod_staff'));
-plugged.on(plugged.MOD_BAN, require('./lib/eventhandlers/mod_ban'));
-plugged.on(plugged.MOD_SKIP, require('./lib/eventhandlers/mod_skip'));
-plugged.on(plugged.MOD_ADD_DJ, require('./lib/eventhandlers/mod_add_dj'));
+fs.readdir('./lib/eventhandlers', function (err, files) {
+    if (err) {
+        story.fatal('Cannot load eventhandlers.', {attach: err});
+        process.exit(1);
+    } else {
+        files.forEach(function (file) {
+            try {
+                var h = require('./lib/eventhandlers/' + file);
+                if (Array.isArray(h.event)) {
+                    h.event.forEach(function (event) {
+                        plugged.on(event, h.handler);
+                    });
+                } else plugged.on(h.event, h.handler);
+                story.debug('EventHandlers', 'Loaded Handler for Event ' + (Array.isArray(h.event) ? h.event.join() : h.event));
+            } catch (e) {
+                story.error('Failed to load eventhandler ' + file, {attach: e});
+                process.exit(1);
+            }
+        });
+    }
+});
 
 module.exports = {plugged: plugged, app: (config.web.enabled ? require('./web/index').app : null)};
